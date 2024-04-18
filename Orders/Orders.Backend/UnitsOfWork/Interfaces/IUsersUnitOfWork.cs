30f199f9b7258443f85561db409d02e136662165
fadagoberto <fadagoberto@gmail.com>
2024-04-18 08:39:17 -0300

    Security from backend

--- Orders/Orders.Backend/UnitsOfWork/Interfaces/IUsersUnitOfWork.cs
+++ Orders/Orders.Backend/UnitsOfWork/Interfaces/IUsersUnitOfWork.cs
@@ -12,8 +12,10 @@ namespace Orders.Backend.UnitsOfWork.Interfaces
         Task<IdentityResult> AddUserAsync(User user, string password);
         Task CheckRoleAsync(string roleName);
         Task AddUserToRoleAsync(User user, string roleName);
-        Task<bool> IsUserInRoleAsync(User user, string roleName); 
-        
+        Task<bool> IsUserInRoleAsync(User user, string roleName);
+        Task<SignInResult> LoginAsync(LoginDTO model);
+        Task LogoutAsync();
+
 
     }
 }
